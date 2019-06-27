a4e1c13166abf14ddef5bf257c132d64f1fafa7c
Georgia Rwechungura <grwechungura@aimgroup.co.tz>
2019-06-27 17:28:10 +0300

    Querrying more data from Content Provider

--- app/src/main/java/com/jwhh/jim/notekeeper/MainActivity.java
+++ app/src/main/java/com/jwhh/jim/notekeeper/MainActivity.java
@@ -49,6 +49,7 @@ import java.util.List;
 
 import static com.jwhh.jim.notekeeper.NoteActivity.LOADER_NOTES;
 import static com.jwhh.jim.notekeeper.NoteKeeperDatabaseContract.*;
+import static com.jwhh.jim.notekeeper.NoteKeeperProviderContract.*;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, LoaderManager.LoaderCallbacks<Cursor> {
@@ -281,34 +282,26 @@ public class MainActivity extends AppCompatActivity
 
         CursorLoader loader=null;
             if(id==LOADER_NOTES){
-                loader=new CursorLoader(this){
-                    @Override
-                    public Cursor loadInBackground() {
 
-                        SQLiteDatabase db=mDbOpenHelper.getReadableDatabase();
 
-                        final String noteOrderBy= CourseInfoEntry.COLUMN_COURSE_TITLE  + "," + NoteInfoEntry.COLUMN_NOTE_TITLE;
+                        final String noteOrderBy= Notes.COLUMN_COURSE_TITLE  +
+                                "," + Notes.COLUMN_NOTE_TITLE;
 
 
                         String noteColums[]={
-                                NoteInfoEntry.getQName(NoteInfoEntry._ID),
-                                NoteInfoEntry.COLUMN_NOTE_TITLE,
-                                CourseInfoEntry.COLUMN_COURSE_TITLE
+                                Notes._ID,
+                                Notes.COLUMN_NOTE_TITLE,
+                                Notes.COLUMN_COURSE_TITLE
 
                         };
 
+
                         //note_info JOIN course_info ON note_info.course_id = course_info.course.id
-                        String tablesWithJoin= NoteInfoEntry.TABLE_NAME + " JOIN " +
-                                CourseInfoEntry.TABLE_NAME + " ON " +
-                                NoteInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID) + " = " +
-                                CourseInfoEntry.getQName(CourseInfoEntry.COLUMN_COURSE_ID);
-
-
-                        return db.query(tablesWithJoin,noteColums,null,null,null,null,noteOrderBy);
+                 loader=new CursorLoader(this, Notes.CONTENT_EXPANDED_URI,noteColums,null,null,noteOrderBy);
 
                     }
-                };
-            }
+
+
         return loader;
     }
 
